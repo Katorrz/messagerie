05339174e1552a006a439c6a27fcfcc64cbd2e31
Katorrz <morandbrice@gmail.com>
2024-11-21 09:40:01 +0100

    navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { ChannelsComponent } from './channels/channels.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HomeComponent],
+  imports: [RouterOutlet,RouterModule,HomeComponent,ChannelsComponent,ProfileComponent,LoginComponent,RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
